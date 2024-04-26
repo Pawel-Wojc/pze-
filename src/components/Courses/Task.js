6b29354ef12b10dc866531f6746b3561f8f43d0d
pawel-pawel <pawelitwojcik@gmail.com>
2024-04-26 22:25:11 +0200

    Tasks update

--- src/components/Courses/Task.js
+++ src/components/Courses/Task.js
@@ -1,47 +1,99 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { redirect, useLocation, useNavigate, useParams } from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import UploadFile from './UploadFile';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
 export default function Task() {
-
-
+  let config = {
+    headers: {
+      Authorization: "Bearer " + sessionStorage.getItem("user_jwt")
+    }
+  }
   let { task_id } = useParams();
 
   const [modalShow, setModalShow] = useState(false);
+  const [sendDisable, setsendDisable] = useState(true);
+  useEffect ()
 
+  const getData = async () => {
+    const { data } = await axios.get(localStorage.getItem("api_path") + "get/task/" + task_id, config)
+      .then(res => {
+        return res;
+      })
+      .catch(err => {
+        console.error(err);
+      })
+    return data
+  }
+
+  const setButton= ()=>{
+    //console.log("tutaj " + data)
+    const date = new Date();
+    const currentDate = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
+    const startDate = new Date(data.date_of_start)
+    const endDate = new Date(data.date_of_end)
+    
+    if(startDate<currentDate<endDate){
+      setsendDisable(false)
+      console.log(false)
+    }else {
+      setsendDisable(true)
+      console.log(true)
+    }
+  }
+
+  const { isSuccess, isLoading, isError, error, data } = useQuery('users', getData, {onCompleted:setButton, refetchOnWindowFocus:false})
+  if (isLoading) {
+    console.log("loading")
+    return <div>Loading..</div>
+  }
+  if (isError) {
+    return <div>Errror, {error.message}</div>
+  }
+  if (!isLoading){
+    //console.log(data)
+    console.log("finished loading")
+  }
+ 
 
   return (
     <>
-      <div class="row justify-content-md-center" style={{}}>
-
+      <div class="row justify-content-md-center">
         <div class="card" style={{ width: '40rem', margin: '10px' }}>
           <div class="card-body">
-            <h5 class="card-title">Tytuł zadanioa Zadanie {task_id}</h5>
-
-
+            <h5 class="card-title">{data.title} 
+            <Button disabled={sendDisable} variant="primary" onClick={() => setModalShow(true)}>
+                  Send Files
+            </Button>
+            </h5>
+            
             <div class="row">
               <div class="col">
-                <h6 >Tutaj bedzie opis zadania </h6>
-                <h9>Wymagania : data od data do <br></br> Jakie pliki ile </h9>
+                
+                <h5 >{data.contents} </h5>
+                <h6>Wymagane od: {data.date_of_start} do:  {data.date_of_end}<br></br> Jakie pliki ile </h6>
 
               </div>
               <div class="col">
-                <Button variant="primary" onClick={() => setModalShow(true)}>
-                  Send Files
-                </Button>
+                
 
-                <UploadFile
+                 <UploadFile
+                  title= {data.title} 
+                  extensions = {data.available_file_extensions}
                   show={modalShow}
                   onHide={() => setModalShow(false)}
-                />
+                /> 
 
               </div>
             </div>
           </div>
         </div>
       </div>
+
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
@@ -12,7 +13,7 @@ root.render(
   
   <BrowserRouter>  
     <Routes> 
-        <Route path="/*" element={<App/>}/>
+        <Route path="/*" element={<QueryClientProvider client={new QueryClient}><App/> </QueryClientProvider>}/>
     </Routes>
   </BrowserRouter>
 );
