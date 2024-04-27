287c5aec960f2ef3a3721b1a9c80701f76a7a707
pawel-pawel <pawelitwojcik@gmail.com>
2024-04-27 16:36:42 +0200

    send button

--- src/components/Courses/Task.js
+++ src/components/Courses/Task.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { redirect, useLocation, useNavigate, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
 import UploadFile from './UploadFile';
 import { useQuery } from 'react-query';
 import axios from 'axios';
@@ -17,7 +15,7 @@ export default function Task() {
 
   const [modalShow, setModalShow] = useState(false);
   const [sendDisable, setsendDisable] = useState(true);
-  useEffect ()
+  
 
   const getData = async () => {
     const { data } = await axios.get(localStorage.getItem("api_path") + "get/task/" + task_id, config)
@@ -25,39 +23,54 @@ export default function Task() {
         return res;
       })
       .catch(err => {
-        console.error(err);
+       // console.error(err);
       })
     return data
   }
 
-  const setButton= ()=>{
-    //console.log("tutaj " + data)
-    const date = new Date();
-    const currentDate = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
-    const startDate = new Date(data.date_of_start)
-    const endDate = new Date(data.date_of_end)
-    
-    if(startDate<currentDate<endDate){
-      setsendDisable(false)
-      console.log(false)
-    }else {
-      setsendDisable(true)
-      console.log(true)
-    }
-  }
+  
 
-  const { isSuccess, isLoading, isError, error, data } = useQuery('users', getData, {onCompleted:setButton, refetchOnWindowFocus:false})
+  
+
+  const {  isLoading, isError, error, data } = useQuery('users', getData, {refetchOnWindowFocus:false})
+  
+  
+  useEffect(()=>{
+    
+      const date = new Date();
+      //const currentDate = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
+      const currentDate = new Date();
+      const startDate = new Date(data?.date_of_start)
+      const endDate = new Date(data?.date_of_end)  
+      if(currentDate >= startDate && currentDate <= endDate){
+        console.log("data sie miesci, od: "+ startDate + " do: " + endDate)
+        setsendDisable(false)
+      }else {
+        console.log("data sie nie miesci")
+        
+        setsendDisable(true)
+      }
+      console.log("finished loading" +sendDisable )
+    
+   
+  },[data])
+  
+  
+  
+  
+  
   if (isLoading) {
     console.log("loading")
     return <div>Loading..</div>
   }
   if (isError) {
+    console.log("error")
     return <div>Errror, {error.message}</div>
   }
-  if (!isLoading){
-    //console.log(data)
-    console.log("finished loading")
-  }
+  
+
+  
+  
  
 
   return (
@@ -69,25 +82,19 @@ export default function Task() {
             <Button disabled={sendDisable} variant="primary" onClick={() => setModalShow(true)}>
                   Send Files
             </Button>
-            </h5>
-            
+            </h5>     
             <div class="row">
-              <div class="col">
-                
+              <div class="col">   
                 <h5 >{data.contents} </h5>
                 <h6>Wymagane od: {data.date_of_start} do:  {data.date_of_end}<br></br> Jakie pliki ile </h6>
-
               </div>
               <div class="col">
-                
-
                  <UploadFile
                   title= {data.title} 
                   extensions = {data.available_file_extensions}
                   show={modalShow}
                   onHide={() => setModalShow(false)}
                 /> 
-
               </div>
             </div>
           </div>
