7fdb866fb24cced650cb85b2342627f578fcd87c
pawel-pawel <pawelitwojcik@gmail.com>
2024-05-12 21:14:14 +0200

    download/delete tasks

--- src/components/Teacher/TeacherCourseDetails.js
+++ src/components/Teacher/TeacherCourseDetails.js
@@ -7,10 +7,13 @@ import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 import { Modal } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
+import { Toast, ToastContainer } from 'react-bootstrap';
 
 export default function TeacherCourseDetails() {
   let { course_id } = useParams();
-
+  const [showToast, setShowToast] = useState(false);
+  const [toastText, settoastText] = useState("");
+  const [toastVariant, settoastVariant] = useState("success");
 
   // getData for specific course
   const getData = async () => {
@@ -28,7 +31,7 @@ export default function TeacherCourseDetails() {
       })
     return data
   }
-  const {refetch, isLoading, isError, error, data } = useQuery("teacher_course", getData, { cacheTime: 0, refetchOnWindowFocus: false })
+  const { refetch, isLoading, isError, error, data } = useQuery("teacher_course", getData, { cacheTime: 0, refetchOnWindowFocus: false })
 
 
   //addin new task
@@ -40,13 +43,13 @@ export default function TeacherCourseDetails() {
     if (formValue == "") {
       return "Name can't be empty"
     }
-    const task = { 
-      title: formValue, 
-      available_file_extensions:'',
-      contents:'',
-      date_of_end:null,
+    const task = {
+      title: formValue,
+      available_file_extensions: '',
+      contents: '',
+      date_of_end: null,
       date_of_start: new Date(),
-      max_total_files_volume:1,
+      max_total_files_volume: 1,
     };
 
     let config = {
@@ -59,16 +62,103 @@ export default function TeacherCourseDetails() {
       data: task
     }
     const { data } = await axios.request(config).then(res => {
+      if (res.status === 200) {
+        settoastText("Task added")
+        settoastVariant("success")
+        setShowToast(true)
+      } else {
+        settoastText("Something went wrong")
+        settoastVariant("danger")
+        setShowToast(true)
+      }
       handleClose()
-      refetch();
-
+      refetch()
+      setFormValue("")
       return res
     })
       .catch(err => {
+        settoastText("Something went wrong")
+        settoastVariant("danger")
+        setShowToast(true)
         console.error(err);
         return err
       })
     return data
+  }
+
+  //deleting task
+
+  const deleteTask = async (taskId) => {
+    let config = {
+      method: 'delete',
+      url: localStorage.getItem("api_path") + "delete/task/" + taskId,
+      maxBodyLength: Infinity,
+      headers: {
+        Authorization: "Bearer " + sessionStorage.getItem("user_jwt")
+      },
+    }
+    const { data } = await axios.request(config).then(res => {
+      if (res.status === 200) {
+        settoastText("Task deleted")
+        settoastVariant("success")
+        setShowToast(true)
+      } else {
+        settoastText("Something went wrong")
+        settoastVariant("danger")
+        setShowToast(true)
+      }
+      handleClose()
+      refetch()
+      return res
+    })
+      .catch(err => {
+        settoastText("Something went wrong")
+        settoastVariant("danger")
+        setShowToast(true)
+
+        console.error(err);
+        return err
+      })
+    return data
+  }
+
+  //download task files
+  const downloadTask = async (taskId, taskTitle) => {
+    let config = {
+      url: localStorage.getItem("api_path") + "download/task/" + taskId + "/files",
+      responseType: 'blob',
+      method: 'GET',
+      headers: {
+        Authorization: "Bearer " + sessionStorage.getItem("user_jwt")
+      }
+    }
+    await axios.request(config)
+      .then(res => {
+        if (res.status === 200) {
+         const href = window.URL.createObjectURL(res.data);
+        const anchorElement = document.createElement('a');
+         anchorElement.href = href;
+         anchorElement.download = taskTitle+".zip";
+         document.body.appendChild(anchorElement);
+         anchorElement.click();
+         document.body.removeChild(anchorElement);
+         window.URL.revokeObjectURL(href);
+
+
+        } else {
+          settoastText("Something went wrong")
+        settoastVariant("danger")
+        setShowToast(true)
+        }
+        
+
+
+        console.log(res)
+        return res;
+      })
+      .catch(err => {
+        console.error(err);
+      })
   }
 
 
@@ -82,6 +172,11 @@ export default function TeacherCourseDetails() {
 
   return (
     <div className='container'>
+      <ToastContainer position='top-end'>
+        <Toast onClose={() => setShowToast(false)} show={showToast} delay={3000} bg={toastVariant} autohide>
+          <Toast.Header>{toastText}</Toast.Header>
+        </Toast>
+      </ToastContainer>
       <h3>{data.title}</h3>
       <Button variant="success" onClick={() => setModalShow(true)}>Add new</Button> {' '}
       <Modal show={modalShow} onHide={handleClose}>
@@ -114,8 +209,8 @@ export default function TeacherCourseDetails() {
                 </Card.Title>
                 <Card.Text>
                 </Card.Text>
-                <Button variant="primary">Download</Button> {' '}
-                <Button variant="danger">Delete</Button>
+                <Button variant="primary" onClick={() => downloadTask(task.id, task.title)} >Download</Button> {' '}
+                <Button variant="danger" onClick={() => deleteTask(task.id)}>Delete</Button>
               </Card.Body>
             </Card>
           </>
