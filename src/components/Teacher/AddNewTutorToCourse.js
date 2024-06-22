f9c3028aeb8796aba13093632445d9c851c98246
pawel-pawel <pawelitwojcik@gmail.com>
2024-06-22 22:02:12 +0200

    Update AddNewTutorToCourse.js

--- src/components/Teacher/AddNewTutorToCourse.js
+++ src/components/Teacher/AddNewTutorToCourse.js
@@ -1,4 +1,4 @@
-import { React, useEffect, useState, useRef } from "react";
+import { React, useEffect, useState, useRef, useContext } from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import axios from "axios";
@@ -7,6 +7,8 @@ import { Modal } from "react-bootstrap";
 import { Toast, ToastContainer } from "react-bootstrap";
 import Dropdown from "react-bootstrap/Dropdown";
 import Form from "react-bootstrap/Form";
+import { CurrentUserContext } from './../Utils/CurrentUserContext';
+import Loading from "../Utils/Loading";
 
 export default function AddNewTutorToCourse() {
   let { course_id } = useParams();
@@ -15,7 +17,13 @@ export default function AddNewTutorToCourse() {
   const [toastVariant, settoastVariant] = useState("success");
   const [formValue, setFormValue] = useState("");
   const [modalShow, setModalShow] = useState(false);
+  const [tutorToAddId, setTutorToAddId] = useState(null);
   const handleClose = () => setModalShow(false);
+
+  const {
+    currentUser,
+    setCurrentUser
+  } = useContext(CurrentUserContext);
 
   const getTutorList = async () => {
     let config = {
@@ -23,39 +31,45 @@ export default function AddNewTutorToCourse() {
         Authorization: "Bearer " + sessionStorage.getItem("user_jwt"),
       },
     };
-    const { tutors } = await axios
+    const { data } = await axios
       .get(localStorage.getItem("api_path") + "get/all/users", config)
       .then((res) => {
-        return res.data.filter((teacher) => teacher.role === "tutor");
+        return res;
       })
       .catch((err) => {
         console.error(err);
       });
-    return tutors;
+    return data.filter((teacher) => teacher.role === "tutor" && teacher.id !== currentUser.id);
   };
-  const { refetch, isLoading, isError, error, tutors } = useQuery(
+  const { refetch, isLoading, isError, error, data } = useQuery(
     "tutor_list",
     getTutorList,
     { cacheTime: 0, refetchOnWindowFocus: false }
   );
 
-  const addNewTutor = async (e) => {
-    e.preventDefault();
-    const tutor = 0;
+  const addNewTutor = async () => {
+    console.dir(tutorToAddId)
+    if (tutorToAddId === null) {
+      setModalShow(false)
+      return
+    }
+    let tutorObject = data.filter((tutor) => tutor.id == tutorToAddId)[0];
+
+
     let config = {
       method: "post",
-      url: localStorage.getItem("api_path") + "add/task/to/course/" + course_id,
+      url: localStorage.getItem("api_path") + "course/" + course_id + "/add/tutor",
       maxBodyLength: Infinity,
       headers: {
         Authorization: "Bearer " + sessionStorage.getItem("user_jwt"),
       },
-      data: tutor,
+      data: tutorObject,
     };
-    const { data } = await axios
+    const { data2 } = await axios
       .request(config)
       .then((res) => {
         if (res.status === 200) {
-          settoastText("Task added");
+          settoastText("Tutor added");
           settoastVariant("success");
           setShowToast(true);
         } else {
@@ -75,18 +89,19 @@ export default function AddNewTutorToCourse() {
         console.error(err);
         return err;
       });
-    return data;
+    return data2;
   };
 
 
 
   //view
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <Loading></Loading>;
   }
   if (isError) {
     return <div>Errror, {error.message}</div>;
   }
+
 
   return (
     <div style={styles.container}>
@@ -106,7 +121,14 @@ export default function AddNewTutorToCourse() {
           <Modal.Title>Select tutor</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          
+          <Form.Select size="lg" onChange={(e) => setTutorToAddId(e.target.value)}>
+            <option value={null}>Select teacher</option>
+            {data?.map(tutor => (
+              <option key={tutor.id} value={tutor.id}>{tutor.name} {tutor.surname}</option>
+            ))}
+          </Form.Select>
+          <Button variant="success" onClick={addNewTutor}>Add</Button>
+
         </Modal.Body>
       </Modal>
       <Button variant="success" onClick={() => setModalShow(true)} style={styles.button}>
